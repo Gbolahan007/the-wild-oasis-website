96c083c7bd08b3195cd283e8c49f1e119574a30f
Gbolahan007 <lawalomogbolahan08@gmail.com>
2025-05-06 19:02:06 +0100

    updated reservation

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
 import Header from "./_components/Header";
+import { ReservationProvider } from "./_components/ReservationContext";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -27,7 +28,9 @@ export default function RootLayout({ children }) {
         <Header />
 
         <div className="flex-1 grid px-8 py-12">
-          <main className="max-w-7xl w-full  mx-auto">{children}</main>
+          <main className="max-w-7xl w-full  mx-auto">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
