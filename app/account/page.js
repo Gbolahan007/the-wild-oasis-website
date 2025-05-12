6d4b0d441693ff7a96a8f50cec86fff2361bd4aa
Gbolahan007 <lawalomogbolahan08@gmail.com>
2025-05-12 07:50:23 +0100

    updated auth page

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,15 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-function Page() {
+async function Page() {
+  const session = await auth();
+  const firstName = session?.user?.name.split(" ")[0];
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Quam
+      Welcome, {firstName}
     </h2>
   );
 }
